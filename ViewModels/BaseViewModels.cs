d84b455117ec3a9d5f5f5cc70e5e21276f4b1f30
Aleksandr Drozhkin <ad@multisoft.ru>
2020-04-04 23:27:38 +0300

    * [ViewModels] : Добавлен базовый абстрактный класс с реализацией интерфейса

---
+++ ViewModels/BaseViewModels.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using WpfAppPage.Annotations;
+
+namespace WpfAppPage.ViewModels
+{
+
+    public abstract class BaseViewModels: INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged ([CallerMemberName] string propertyName = null)
+        {
+            this.PropertyChanged?.Invoke(sender: this , e: new PropertyChangedEventArgs(propertyName: propertyName));
+        }
+    }
+
+}
--- ViewModels/MainPageViewModels.cs
+++ ViewModels/MainPageViewModels.cs
@@ -4,7 +4,7 @@ using System.Text;
 
 namespace WpfAppPage.ViewModels
 {
-    class MainPageViewModels
+    public class MainPageViewModels : BaseViewModels
     {
     }
 }
